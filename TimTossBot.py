84bfa2d292d1236463c58bce022276961673ac99
Timothy Dusek <timothy.dusek@gmail.com>
2019-05-25 20:30:23 -0500

    Update TimTossBot.py

--- TimTossBot.py
+++ TimTossBot.py
@@ -3,7 +3,7 @@
 
 #This program uses the python-sc2 library to create a bot which can play Starcraft 2
 #Due to some errors in Python v3.7 this program was written using v3.6
-#~165 iterations per minute but this seems to change by day AND by pc
+#~4870 iterations per minute but this changes on each computer who runs this programk
 
 
 #ToDo: start part 6
@@ -13,11 +13,14 @@
 	#for build_assimilators vaspene should be vespene.
 
 import sc2
-from sc2 import run_game, maps, Race, Difficulty
+from sc2 import run_game, maps, Race, Difficulty, game_info
+from sc2.ids.upgrade_id import UpgradeId
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
 CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
-import random
+import random, time
+import cv2
+import numpy as np
 
 class TimTossBot(sc2.BotAI):
 	def __init__(self): #initialization method
@@ -35,8 +38,21 @@ class TimTossBot(sc2.BotAI):
 		await self.expand() #method to build another base
 		await self.offensive_force_buildings() #method to create buildings used to make an army
 		await self.build_offensive_force() #method used to tell buildings what army units to create
+		await self.intel()
 		await self.attack() #method to attack the enemy
 
+
+	async def intel(self):
+		game_data = np.zeros((self.game_info.map_size[1],self.game_info.map_size[0], 3), np.uint8)
+		for nexus in self.units(NEXUS):
+			nex_pos = nexus.position
+			cv2.circle(game_data, (int(nex_pos[0]), int(nex_pos[1])), 10, (0,255,0), -1)
+
+
+		flipped = cv2.flip(game_data, 0) # flipping game data for coordinates
+		resized = cv2.resize(flipped, dsize=None, fx=2, fy=2)
+		cv2.imshow('Intel', resized)
+		cv2.waitKey(1)
 
 	async def build_workers(self):
 		if len(self.units(NEXUS))*16 > len(self.units(PROBE)):
@@ -44,7 +60,7 @@ class TimTossBot(sc2.BotAI):
 				for nexus in self.units(NEXUS).ready:
 					if self.can_afford(PROBE) and nexus.noqueue:
 						await self.do(nexus.train(PROBE))
-
+						time.sleep(0.05)
 
 
 	async def build_pylons(self):
@@ -53,6 +69,7 @@ class TimTossBot(sc2.BotAI):
 			if nexuses.exists:
 				if self.can_afford(PYLON):
 					await self.build(PYLON, near=nexuses.first)
+					time.sleep(0.05)
 
 
 
@@ -67,13 +84,13 @@ class TimTossBot(sc2.BotAI):
 					break
 				if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
 					await self.do(worker.build(ASSIMILATOR, vaspene))
-
+					time.sleep(0.05)
 
 
 	async def expand(self):
 		if self.units(NEXUS).amount < (self.iteration / self.ITERATIONS_PER_MINUTE) and self.can_afford(NEXUS):
 			await self.expand_now()
-			#time.sleep(0.05)
+			time.sleep(0.05)
 
 
 	async def offensive_force_buildings(self):
@@ -84,27 +101,26 @@ class TimTossBot(sc2.BotAI):
 			if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
 				if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
 					await self.build(CYBERNETICSCORE, near=pylon)
+					time.sleep(0.05)
 
-			elif len(self.units(GATEWAY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)/2):
+			elif len(self.units(GATEWAY)) < 1:
 				if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
 					await self.build(GATEWAY, near=pylon)
+					time.sleep(0.05)
 
 			if self.units(CYBERNETICSCORE).ready.exists:
-				if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)/2):
+				if len(self.units(STARGATE)) < (self.iteration / self.ITERATIONS_PER_MINUTE):
 					if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
 						await self.build(STARGATE, near=pylon)
+						time.sleep(0.05)
 
 
 
 	async def build_offensive_force(self):
-		for gw in self.units(GATEWAY).ready.noqueue:
-			if not self.units(STALKER).amount > self.units(VOIDRAY).amount:
-				if self.can_afford(STALKER) and self.supply_left > 0:
-					await self.do(gw.train(STALKER))
-
 		for sg in self.units(STARGATE).ready.noqueue:
 			if self.can_afford(VOIDRAY) and self.supply_left > 0:
 				await self.do(sg.train(VOIDRAY))
+				time.sleep(0.05)
 
 
 	def find_target(self, state):
@@ -120,8 +136,7 @@ class TimTossBot(sc2.BotAI):
 
 	async def attack(self):
 		# {UNIT: [n to fight, n to defend]}
-		aggressive_units = {STALKER: [15, 3],
-							VOIDRAY: [8, 3]}
+		aggressive_units = {VOIDRAY: [8, 3]}
 
 
 		for UNIT in aggressive_units:
